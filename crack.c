85f5c3f879a158a98fc9acb2aa917b7e0b1e4b5f
David Pena <dpena9@student.sierracollege.edu>
2024-11-10 14:37:50 -0800

    Added Binary Search

--- crack.c
+++ crack.c
@@ -11,6 +11,21 @@
 #define PASS_LEN 50     // Maximum length any password will be.
 #define HASH_LEN 33     // Length of hash plus one for null.
 
+int strcomp(const void *a, const void *b){
+
+    char **aa = (char **)a;
+    char **bb = (char **)b;
+
+    return strcmp(*aa, *bb);
+}
+
+int strsearch(const void * target, const void *elem){
+
+    char *tt = (char *) target;
+    char **ee = (char **)elem;
+
+    return strcmp(tt, *ee);
+}
 
 int main(int argc, char *argv[])
 {
@@ -20,7 +35,6 @@ int main(int argc, char *argv[])
         exit(1);
     }
 
-    // TODO: Read the hashes file into an array.
     FILE *hashFile = fopen(argv[1], "r");
     if (!hashFile){
         printf("Can't open hash file.\n");
@@ -32,41 +46,28 @@ int main(int argc, char *argv[])
         printf("Can't open password File,\n");
         exit(1);
     }
-    //   Use either a 2D array or an array of arrays.
-    //   Use the loadFile function from fileutil.c
-    //   Uncomment the appropriate statement.
+    
     int size;
-    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
     char **hashes = loadFileAA(argv[1], &size);
     
-    // CHALLENGE1: Sort the hashes using qsort.
+    qsort(hashes, size, sizeof(char *), strcomp);
     
-    // TODO
-    // Open the password file for reading.
     int count = 0;
     char line[1000];
     while(fgets(line, 1000, passwordFile)){
         
         char *nl = strchr(line, '\n');
         if (nl) *nl = '\0';
-
+        
         char *hashedPass = md5(line, strlen(line));
 
-        char *foundHash = substringSearchAA(hashedPass, hashes, size);
+        char *foundHash = bsearch(hashedPass, hashes, size, sizeof(char *), strsearch);
         if (foundHash){
             printf("Password: %s\tHash: %s\n", line, hashedPass);
             count += 1;
-        }
+        } 
     }
-    // TODO
-    // For each password, hash it, then use the array search
-    // function from fileutil.h to find the hash.
-    // If you find it, display the password and the hash.
-    // Keep track of how many hashes were found.
-    // CHALLENGE1: Use binary search instead of linear search.
 
-    // TODO
-    // When done with the file:
     //   Close the file
     //   Display the number of hashes found.
     //   Free up memory.
